f2e0ed5b8a9ea35e6b65d5495ea4ed9630169734
Md Omar Faruque Sarker <Mdomarfaruque.Sarker@newport.ac.uk>
2009-11-18 15:49:09 +0000

    Added server killer based on PID from ps

---
+++ PlayerServerSide/serverkiller.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import os, signal, subprocess, re, sys
+
+cmd = "ps aux | grep periodic"
+subproc = subprocess.Popen([cmd, ], stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
+output = subproc.communicate()
+output=  output[0].split()
+if (output[10] != 'python'):
+    sys.exit(1)
+print output[1]
+pid = int(output[1])
+if(pid > 0): 
+    os.kill(pid, signal.SIGKILL)
+else:
+    print "PID not OK"
