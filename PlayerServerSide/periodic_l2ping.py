459f2193e144178027e2e6c99f978cd51b527d31
Md Omar Faruque Sarker <Mdomarfaruque.Sarker@newport.ac.uk>
2009-11-20 13:43:27 +0000

    Added signal receiver

--- PlayerServerSide/periodic_l2ping.py
+++ PlayerServerSide/periodic_l2ping.py
@@ -1,99 +1,98 @@
 #!/usr/bin/env python
 
-import time, os, sys, sched, subprocess, re, signal
+import time, os, sys, sched, subprocess, re, signal, traceback
 import gobject, dbus, dbus.service, dbus.mainloop.glib 
-
-
-configfile = './player-server-config.txt'
-bd_addr = ' '
-packet_loss = ' '
-server_exist = 'False'
-server_pid = 0
 
 schedule = sched.scheduler(time.time, time.sleep)
 
+class glb: # gloabl variables
+	robotid = -1
+	configfile = ' '
+	bd_addr = ' '
+	DBUS_PATH = ' '
+	DBUS_IFACE = 'uk.ac.newport.ril.EpuckRunManager'
+	ping_delay = 5 # in sec
+	packet_loss = ' '
+	server_exist = 'False'
+	server_pid = 0
+	
 
-class Example(dbus.service.Object):
+class RobotSignal(dbus.service.Object):
 	def __init__(self, object_path):
 		dbus.service.Object.__init__(self, dbus.SessionBus(), object_path)
 
-	@dbus.service.signal(dbus_interface='com.example.Sample', signature='ss')
+	@dbus.service.signal(dbus_interface= glb.DBUS_IFACE, signature='ss')
 	def RobotState(self, bdaddr, sig):
 		# The signal is emitted when this method exits
 		# You can have code here if you wish
 		#pass
 		print "Now  %s robot signal is: %s" % (bdaddr, sig)
-
 	def Exit(self):
 		loop.quit()
 
 
-
-#Emit DBus-Signal : PlayerServer XXXX (Alive, Killing, Dead)
+#Emit DBus-Signal 
 def emit_dbus_signal(sig):
 	global dbus_signal
-	print "Emitting signal>>> Player server for Epuck " + bd_addr + ": " + sig
-	dbus_signal.RobotState(bd_addr, sig)
+	print "Emitting signal>>> Player server for Epuck " + glb.bd_addr + ": " + sig
+	dbus_signal.RobotState(glb.bd_addr, sig)
 
 def stop_player_server():
-	global server_pid, server_exist
 	emit_dbus_signal('Terminating')
-	#time.sleep(3.0)
-	print "\t Killling server >>> PID: ", server_pid
-	
-	if(server_pid > 0):
-		os.kill(server_pid, signal.SIGKILL)
-		server_exist = 'False'
+	#time.sleep(1.0)
+	print "\t Killling server >>> PID: ", glb.server_pid
+	if(glb.server_pid > 0):
+		os.kill(glb.server_pid, signal.SIGKILL)
+		glb.server_exist = 'False'
 		emit_dbus_signal('Killed')	
 
 def get_config(config):
-	global bd_addr, configfile
 	result = ' '
-	f = open(configfile, 'r')
+	f = open(glb.configfile, 'r')
 	data = f.read()
 	lst = re.split(';', data)
-	if (lst[0] != bd_addr):
-		print "BD ADDR does not match"
-		return result
-	if(config == 'port'):
+	if(int(lst[0]) != glb.robotid):
+		print "Robot ID does not match"
+	if (config == 'bdaddr'):
 		result = lst[1]
-	if(config == 'cfgfile'):
+	if(config == 'port'):
 		result = lst[2]
+	if(config == 'cfgfile'):
+		result = lst[3]
 	return result
  
 def start_player_server():
-	global server_exist, server_pid
 	port = get_config('port')
 	cfgfile = get_config('cfgfile')
 	cmd = "/usr/bin/robot-player -q -p " + port + " " + cfgfile + " " + "&"
 	subproc = subprocess.Popen([cmd, ], stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
 	#stdout_value = subproc.communicate()
-	#pscmd = "/bin/ps aux | grep robot-player"
 	print '\t at time:', time.time()
 	print "\t sever started"
 	#print '\t got player stdout_value:', stdout_value[0]
-	server_pid = subproc.pid + 1 # Strange Fix! 
-	print "\t server PID: ", server_pid
-	server_exist = 'True'
+	glb.server_pid = subproc.pid + 1 # Strange bug fixed: this is actual PID  
+	print "\t server PID: ", glb.server_pid
+	glb.server_exist = 'True'
 	emit_dbus_signal('Alive')
 
 def process_ping_output(output):
-	global packet_loss, server_exist
+	#global packet_loss, server_exist
 	pct = re.findall(r"\d\%", output)   
 	if(pct):   
 		print "%loss: ", pct[0]
-		packet_loss = pct[0]
-	if (packet_loss == '0%'):
+		glb.packet_loss = pct[0]
+	if (glb.packet_loss == '0%'):
 		print "Device Alive"
 		# start player-server, if not started yet        
-		if(server_exist == 'False'):
+		if(glb.server_exist == 'False'):
 			start_player_server()
-	if (packet_loss == '100%' or packet_loss == ' '):
+	if (glb.packet_loss == '100%' or glb.packet_loss == ' '):
 		print "Device Dead"
+		emit_dbus_signal('Dead')	
 		# kill player-server, if already exist
-		if(server_exist == 'True'):
+		if(glb.server_exist == 'True'):
 			stop_player_server()
-	packet_loss = ' '
+	glb.packet_loss = ' '
 
 def perform_ping(bdaddr, inc):
     schedule.enter(inc, 0, perform_ping, (bdaddr, inc)) # re-schedule to make it periodic
@@ -105,35 +104,40 @@ def perform_ping(bdaddr, inc):
     #print '\t got l2ping stdout_value:', stdout_value[0]
     process_ping_output(stdout_value[0])
 
-def main(bdaddr, inc=5):
-	global bd_addr
-	bd_addr = bdaddr
-		
-	schedule.enter(0, 0, perform_ping, (bdaddr, inc))
-	schedule.run()
-	loop.run()
+def main():
+	global dbus_signal
+	try:	
+		e = schedule.enter(0, 0, perform_ping, (glb.bd_addr, glb.ping_delay))
+		schedule.run()
+		loop.run()
+	except (KeyboardInterrupt, dbus.DBusException, SystemExit):
+		print "User requested exit... shutting down now"
+		dbus_signal.Exit()
+		pass				
+	  	sys.exit(0)
 
 if __name__ == '__main__':
 	dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
-
 	session_bus = dbus.SessionBus()
-	name = dbus.service.BusName('com.example.Sample', session_bus)
-	#object = TestObject(session_bus)
-
-	dbus_signal = Example('/robotsignal')
-	loop = gobject.MainLoop()
-	print "Running example signal emitter service."
-	#print usage	
-
 
 	numargs = len(sys.argv) - 1
-	if numargs < 1 or numargs > 2:
-		print "usage:" + sys.argv[0] + "bdaddr [sec delay]"
+	if numargs > 1 or numargs < 1:
+		print "usage:" + sys.argv[0] + "<Robot ID>"
 		sys.exit(1)
-	bdaddr = sys.argv[1]
-	if numargs < 3:
-		main(bdaddr)
 	else:
-		inc = int(sys.argv[2])
-		main(bdaddr, inc)
+		glb.robotid = int(sys.argv[1])
+		glb.configfile = './robot'+sys.argv[1]+'.txt' # cfg file as "robot<X>.txt"
+		glb.bd_addr = get_config('bdaddr')
+		glb.DBUS_PATH = '/robot'+sys.argv[1]
+
+	try:
+		name = dbus.service.BusName(glb.DBUS_IFACE, session_bus)
+		dbus_signal = RobotSignal(glb.DBUS_PATH)
+		loop = gobject.MainLoop()
+		print "Running example signal emitter service."	
+	except dbus.DBusException:
+		traceback.print_exc()
+		sys.exit(1)	
+
+	main()
 
--- PlayerServerSide/serverkiller.py
+++ PlayerServerSide/serverkiller.py
@@ -6,7 +6,7 @@ cmd = "ps aux | grep periodic"
 subproc = subprocess.Popen([cmd, ], stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
 output = subproc.communicate()
 output=  output[0].split()
-if (output[10] != 'python'):
+if (output[11] != './periodic_l2ping.py'):
     sys.exit(1)
 print output[1]
 pid = int(output[1])
